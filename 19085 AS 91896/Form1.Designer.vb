833f89c4ec29a3334876c72cee943f26fe649c9b
19085 <19085@jpc.school.nz>
2023-03-17 09:26:59 +1300

    final design, with correct naming, variables defined, and delivery subroutine created.

--- 19085 AS 91896/Form1.Designer.vb
+++ 19085 AS 91896/Form1.Designer.vb
@@ -22,10 +22,544 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        ChkDelivery = New CheckBox()
+        TxtName = New TextBox()
+        TxtStreetNumber = New TextBox()
+        TxtPhone = New TextBox()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        UpdC = New NumericUpDown()
+        Label10 = New Label()
+        UpdV = New NumericUpDown()
+        UpdH = New NumericUpDown()
+        UpdHc = New NumericUpDown()
+        UpdM = New NumericUpDown()
+        UpdP = New NumericUpDown()
+        UpdB = New NumericUpDown()
+        Label11 = New Label()
+        Label12 = New Label()
+        Label13 = New Label()
+        Label14 = New Label()
+        Label15 = New Label()
+        Label16 = New Label()
+        UpdIv = New NumericUpDown()
+        UpdIh = New NumericUpDown()
+        UpdIm = New NumericUpDown()
+        UpdIp = New NumericUpDown()
+        UpdIc = New NumericUpDown()
+        Label17 = New Label()
+        Label18 = New Label()
+        Label19 = New Label()
+        Label20 = New Label()
+        Label21 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Label22 = New Label()
+        Label23 = New Label()
+        TxtSuburb = New TextBox()
+        TxtStreetName = New TextBox()
+        CType(UpdC, ComponentModel.ISupportInitialize).BeginInit()
+        CType(UpdV, ComponentModel.ISupportInitialize).BeginInit()
+        CType(UpdH, ComponentModel.ISupportInitialize).BeginInit()
+        CType(UpdHc, ComponentModel.ISupportInitialize).BeginInit()
+        CType(UpdM, ComponentModel.ISupportInitialize).BeginInit()
+        CType(UpdP, ComponentModel.ISupportInitialize).BeginInit()
+        CType(UpdB, ComponentModel.ISupportInitialize).BeginInit()
+        CType(UpdIv, ComponentModel.ISupportInitialize).BeginInit()
+        CType(UpdIh, ComponentModel.ISupportInitialize).BeginInit()
+        CType(UpdIm, ComponentModel.ISupportInitialize).BeginInit()
+        CType(UpdIp, ComponentModel.ISupportInitialize).BeginInit()
+        CType(UpdIc, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(120, 8)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(0, 15)
+        Label1.TabIndex = 0
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.Location = New Point(3, 8)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(199, 28)
+        Label2.TabIndex = 1
+        Label2.Text = "Rotorua Dream Pizza "' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label3.Location = New Point(3, 46)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(55, 21)
+        Label3.TabIndex = 2
+        Label3.Text = "Name:"' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label4.Location = New Point(3, 126)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(99, 21)
+        Label4.TabIndex = 3
+        Label4.Text = "Street Name:"' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label5.Location = New Point(3, 135)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(0, 21)
+        Label5.TabIndex = 4
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label6.Location = New Point(3, 74)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(119, 21)
+        Label6.TabIndex = 5
+        Label6.Text = "Phone Number:"' 
+        ' ChkDelivery
+        ' 
+        ChkDelivery.AutoSize = True
+        ChkDelivery.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        ChkDelivery.Location = New Point(3, 98)
+        ChkDelivery.Name = "ChkDelivery"
+        ChkDelivery.RightToLeft = RightToLeft.Yes
+        ChkDelivery.Size = New Size(93, 25)
+        ChkDelivery.TabIndex = 6
+        ChkDelivery.Text = "?Delivery"
+        ChkDelivery.TextAlign = ContentAlignment.MiddleCenter
+        ChkDelivery.UseVisualStyleBackColor = True
+        ' 
+        ' TxtName
+        ' 
+        TxtName.Location = New Point(64, 48)
+        TxtName.Name = "TxtName"
+        TxtName.Size = New Size(307, 23)
+        TxtName.TabIndex = 7
+        ' 
+        ' TxtStreetNumber
+        ' 
+        TxtStreetNumber.Enabled = False
+        TxtStreetNumber.Location = New Point(124, 153)
+        TxtStreetNumber.Name = "TxtStreetNumber"
+        TxtStreetNumber.Size = New Size(256, 23)
+        TxtStreetNumber.TabIndex = 8
+        ' 
+        ' TxtPhone
+        ' 
+        TxtPhone.Location = New Point(128, 77)
+        TxtPhone.Name = "TxtPhone"
+        TxtPhone.Size = New Size(243, 23)
+        TxtPhone.TabIndex = 9
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label7.Location = New Point(3, 224)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(183, 21)
+        Label7.TabIndex = 10
+        Label7.Text = "Regular pizzas ($8.50 ea)"' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label8.Location = New Point(120, 371)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(0, 21)
+        Label8.TabIndex = 11
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label9.Location = New Point(3, 392)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(200, 21)
+        Label9.TabIndex = 12
+        Label9.Text = "Gourmet pizzas ($12.50 ea)"' 
+        ' UpdC
+        ' 
+        UpdC.Location = New Point(151, 257)
+        UpdC.Name = "UpdC"
+        UpdC.Size = New Size(35, 23)
+        UpdC.TabIndex = 13
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Font = New Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point)
+        Label10.Location = New Point(3, 260)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(59, 20)
+        Label10.TabIndex = 14
+        Label10.Text = "Cheese:"' 
+        ' UpdV
+        ' 
+        UpdV.Location = New Point(336, 315)
+        UpdV.Name = "UpdV"
+        UpdV.Size = New Size(35, 23)
+        UpdV.TabIndex = 15
+        ' 
+        ' UpdH
+        ' 
+        UpdH.Location = New Point(336, 286)
+        UpdH.Name = "UpdH"
+        UpdH.Size = New Size(35, 23)
+        UpdH.TabIndex = 16
+        ' 
+        ' UpdHc
+        ' 
+        UpdHc.Location = New Point(336, 257)
+        UpdHc.Name = "UpdHc"
+        UpdHc.Size = New Size(35, 23)
+        UpdHc.TabIndex = 17
+        ' 
+        ' UpdM
+        ' 
+        UpdM.Location = New Point(151, 344)
+        UpdM.Name = "UpdM"
+        UpdM.Size = New Size(35, 23)
+        UpdM.TabIndex = 18
+        ' 
+        ' UpdP
+        ' 
+        UpdP.Location = New Point(151, 315)
+        UpdP.Name = "UpdP"
+        UpdP.Size = New Size(35, 23)
+        UpdP.TabIndex = 19
+        ' 
+        ' UpdB
+        ' 
+        UpdB.Location = New Point(151, 286)
+        UpdB.Name = "UpdB"
+        UpdB.Size = New Size(35, 23)
+        UpdB.TabIndex = 20
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Font = New Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point)
+        Label11.Location = New Point(192, 289)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(74, 20)
+        Label11.TabIndex = 21
+        Label11.Text = "Hawaiian:"' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Font = New Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point)
+        Label12.Location = New Point(192, 318)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(83, 20)
+        Label12.TabIndex = 22
+        Label12.Text = "Vegetarian:"' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.Font = New Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point)
+        Label13.Location = New Point(192, 260)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(124, 20)
+        Label13.TabIndex = 23
+        Label13.Text = "Ham and Cheese:"' 
+        ' Label14
+        ' 
+        Label14.AutoSize = True
+        Label14.Font = New Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point)
+        Label14.Location = New Point(3, 342)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(85, 20)
+        Label14.TabIndex = 24
+        Label14.Text = "Meatlovers:"' 
+        ' Label15
+        ' 
+        Label15.AutoSize = True
+        Label15.Font = New Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point)
+        Label15.Location = New Point(3, 315)
+        Label15.Name = "Label15"
+        Label15.Size = New Size(79, 20)
+        Label15.TabIndex = 25
+        Label15.Text = "Pepperoni:"' 
+        ' Label16
+        ' 
+        Label16.AutoSize = True
+        Label16.Font = New Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point)
+        Label16.Location = New Point(3, 289)
+        Label16.Name = "Label16"
+        Label16.Size = New Size(113, 20)
+        Label16.TabIndex = 26
+        Label16.Text = "Beef and onion:"' 
+        ' UpdIv
+        ' 
+        UpdIv.Location = New Point(336, 456)
+        UpdIv.Name = "UpdIv"
+        UpdIv.Size = New Size(35, 23)
+        UpdIv.TabIndex = 27
+        ' 
+        ' UpdIh
+        ' 
+        UpdIh.Location = New Point(336, 427)
+        UpdIh.Name = "UpdIh"
+        UpdIh.Size = New Size(35, 23)
+        UpdIh.TabIndex = 28
+        ' 
+        ' UpdIm
+        ' 
+        UpdIm.Location = New Point(151, 485)
+        UpdIm.Name = "UpdIm"
+        UpdIm.Size = New Size(35, 23)
+        UpdIm.TabIndex = 29
+        ' 
+        ' UpdIp
+        ' 
+        UpdIp.Location = New Point(151, 456)
+        UpdIp.Name = "UpdIp"
+        UpdIp.Size = New Size(35, 23)
+        UpdIp.TabIndex = 30
+        ' 
+        ' UpdIc
+        ' 
+        UpdIc.Location = New Point(151, 427)
+        UpdIc.Name = "UpdIc"
+        UpdIc.Size = New Size(35, 23)
+        UpdIc.TabIndex = 31
+        ' 
+        ' Label17
+        ' 
+        Label17.AutoSize = True
+        Label17.Font = New Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point)
+        Label17.Location = New Point(192, 454)
+        Label17.Name = "Label17"
+        Label17.Size = New Size(128, 20)
+        Label17.TabIndex = 32
+        Label17.Text = "Italian Vegetarian:"' 
+        ' Label18
+        ' 
+        Label18.AutoSize = True
+        Label18.Font = New Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point)
+        Label18.Location = New Point(192, 425)
+        Label18.Name = "Label18"
+        Label18.Size = New Size(119, 20)
+        Label18.TabIndex = 33
+        Label18.Text = "Italian Hawaiian:"' 
+        ' Label19
+        ' 
+        Label19.AutoSize = True
+        Label19.Font = New Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point)
+        Label19.Location = New Point(3, 485)
+        Label19.Name = "Label19"
+        Label19.Size = New Size(130, 20)
+        Label19.TabIndex = 34
+        Label19.Text = "Italian Meatlovers:"' 
+        ' Label20
+        ' 
+        Label20.AutoSize = True
+        Label20.Font = New Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point)
+        Label20.Location = New Point(3, 454)
+        Label20.Name = "Label20"
+        Label20.Size = New Size(124, 20)
+        Label20.TabIndex = 35
+        Label20.Text = "Italian Pepperoni:"' 
+        ' Label21
+        ' 
+        Label21.AutoSize = True
+        Label21.Font = New Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point)
+        Label21.Location = New Point(3, 425)
+        Label21.Name = "Label21"
+        Label21.Size = New Size(104, 20)
+        Label21.TabIndex = 36
+        Label21.Text = "Italian Cheese:"' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.LimeGreen
+        Button1.Location = New Point(12, 518)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(55, 55)
+        Button1.TabIndex = 37
+        Button1.Text = "Next"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.Red
+        Button2.Location = New Point(245, 518)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(55, 55)
+        Button2.TabIndex = 38
+        Button2.Text = "Restart"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.Red
+        Button3.Location = New Point(316, 518)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(55, 55)
+        Button3.TabIndex = 39
+        Button3.Text = "Quit"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Label22
+        ' 
+        Label22.AutoSize = True
+        Label22.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label22.Location = New Point(3, 180)
+        Label22.Name = "Label22"
+        Label22.Size = New Size(64, 21)
+        Label22.TabIndex = 40
+        Label22.Text = "Suburb:"' 
+        ' Label23
+        ' 
+        Label23.AutoSize = True
+        Label23.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label23.Location = New Point(3, 153)
+        Label23.Name = "Label23"
+        Label23.Size = New Size(115, 21)
+        Label23.TabIndex = 41
+        Label23.Text = "Street Number:"' 
+        ' TxtSuburb
+        ' 
+        TxtSuburb.Enabled = False
+        TxtSuburb.Location = New Point(73, 182)
+        TxtSuburb.Name = "TxtSuburb"
+        TxtSuburb.Size = New Size(307, 23)
+        TxtSuburb.TabIndex = 42
+        ' 
+        ' TxtStreetName
+        ' 
+        TxtStreetName.Enabled = False
+        TxtStreetName.Location = New Point(108, 124)
+        TxtStreetName.Name = "TxtStreetName"
+        TxtStreetName.Size = New Size(272, 23)
+        TxtStreetName.TabIndex = 43
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(383, 585)
+        Controls.Add(TxtStreetName)
+        Controls.Add(TxtSuburb)
+        Controls.Add(Label23)
+        Controls.Add(Label22)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label21)
+        Controls.Add(Label20)
+        Controls.Add(Label19)
+        Controls.Add(Label18)
+        Controls.Add(Label17)
+        Controls.Add(UpdIc)
+        Controls.Add(UpdIp)
+        Controls.Add(UpdIm)
+        Controls.Add(UpdIh)
+        Controls.Add(UpdIv)
+        Controls.Add(Label16)
+        Controls.Add(Label15)
+        Controls.Add(Label14)
+        Controls.Add(Label13)
+        Controls.Add(Label12)
+        Controls.Add(Label11)
+        Controls.Add(UpdB)
+        Controls.Add(UpdP)
+        Controls.Add(UpdM)
+        Controls.Add(UpdHc)
+        Controls.Add(UpdH)
+        Controls.Add(UpdV)
+        Controls.Add(Label10)
+        Controls.Add(UpdC)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(TxtPhone)
+        Controls.Add(TxtStreetNumber)
+        Controls.Add(TxtName)
+        Controls.Add(ChkDelivery)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(UpdC, ComponentModel.ISupportInitialize).EndInit()
+        CType(UpdV, ComponentModel.ISupportInitialize).EndInit()
+        CType(UpdH, ComponentModel.ISupportInitialize).EndInit()
+        CType(UpdHc, ComponentModel.ISupportInitialize).EndInit()
+        CType(UpdM, ComponentModel.ISupportInitialize).EndInit()
+        CType(UpdP, ComponentModel.ISupportInitialize).EndInit()
+        CType(UpdB, ComponentModel.ISupportInitialize).EndInit()
+        CType(UpdIv, ComponentModel.ISupportInitialize).EndInit()
+        CType(UpdIh, ComponentModel.ISupportInitialize).EndInit()
+        CType(UpdIm, ComponentModel.ISupportInitialize).EndInit()
+        CType(UpdIp, ComponentModel.ISupportInitialize).EndInit()
+        CType(UpdIc, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents ChkDelivery As CheckBox
+    Friend WithEvents TxtName As TextBox
+    Friend WithEvents TxtStreetNumber As TextBox
+    Friend WithEvents TxtPhone As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents UpdC As NumericUpDown
+    Friend WithEvents Label10 As Label
+    Friend WithEvents UpdV As NumericUpDown
+    Friend WithEvents UpdH As NumericUpDown
+    Friend WithEvents UpdHc As NumericUpDown
+    Friend WithEvents UpdM As NumericUpDown
+    Friend WithEvents UpdP As NumericUpDown
+    Friend WithEvents UpdB As NumericUpDown
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents UpdIv As NumericUpDown
+    Friend WithEvents UpdIh As NumericUpDown
+    Friend WithEvents UpdIm As NumericUpDown
+    Friend WithEvents UpdIp As NumericUpDown
+    Friend WithEvents UpdIc As NumericUpDown
+    Friend WithEvents Label17 As Label
+    Friend WithEvents Label18 As Label
+    Friend WithEvents Label19 As Label
+    Friend WithEvents Label20 As Label
+    Friend WithEvents Label21 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label22 As Label
+    Friend WithEvents Label23 As Label
+    Friend WithEvents TxtSuburb As TextBox
+    Friend WithEvents TxtStreetName As TextBox
 End Class
--- 19085 AS 91896/Form1.vb
+++ 19085 AS 91896/Form1.vb
@@ -1,3 +1,14 @@
 ﻿Public Class Form1
+    Const PIZZACOST As Decimal = 8.5
+    Const GPIZZACOST As Decimal = 5
+    Const DELIVERYFEE As Decimal = 3
+    Dim Address(2) As String
+    Dim Customerdetails(1) As String
+    Dim Pizzascount(11) As Integer
+    Dim Delivery As Boolean
 
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles ChkDelivery.CheckedChanged, ChkDelivery.CheckedChanged, ChkDelivery.CheckedChanged
+        Delivery = Not Delivery
+        TxtStreetNumber.Enabled = Not TxtStreetNumber.Enabled
+    End Sub
 End Class
